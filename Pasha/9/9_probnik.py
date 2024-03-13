b5841903fd22c8cf4b045698c8b60e27e6e9bd9f
Benhooky <egorkulygin444@gmail.com>
2024-03-13 19:55:13 +0300

    1

---
+++ Pasha/9/9_probnik.py
@@ -0,0 +1,20 @@
+f = open("ItEge/Pasha/9/9probnik.txt")
+lis = [list(map(int, x.split())) for x in f]
+listOfColums = [[x[i] for x in lis] for i in range(5)]
+lisOfDict = [dict1 := {}, dict2 := {}, dict3 := {}, dict4 := {}, dict5 := {}]
+for dictIndex, currentDict in enumerate(lisOfDict):#5 проходов
+    for i in listOfColums[dictIndex]:#16000 проходов
+        if i not in currentDict.keys():
+            currentDict[i] = listOfColums[dictIndex].count(i)
+answer = 0
+for number,j in  enumerate(lis,1):
+    cntOk = 0
+    cntSame = 0
+    for l in range(len(j)):
+        if any(x % 2 == 0 for x in j[l + 1:]):
+            cntOk += 1
+        if j.count(j[l]) == lisOfDict[l][j[l]]:
+            cntSame +=1
+    if cntOk >= 3 and cntSame >= 1:
+        answer = number
+print(answer)
--- Sasha/17/12471.py
+++ Sasha/17/12471.py
@@ -14,6 +14,14 @@ for i in lis:
         max13 = max(max13,i)
 for i in range(len(lis)-2):
     helper = [lis[i],lis[i+1],lis[i+2]]
+    #1
+    '''
+    if all(x%2==0 for x in helper) or any(len(str(abs(x)))==2 for x in helper):
+        if sum(helper)<=max13:
+            cnt+=1
+            avgSum+=sum(helper)
+    '''
+    #2
     if (helper[0]%2==0 and helper[1]%2==0 and helper[2]%2==0) or len(str(abs(helper[0])))==2 or len(str(abs(helper[1])))==2 or len(str(abs(helper[2])))==2:
         if sum(helper)<=max13:
             cnt+=1
