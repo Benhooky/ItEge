a97873e8ed5378c7d96d262a4d6224d13c92847f
Benhooky <egorkulygin444@gmail.com>
2024-05-02 23:19:55 +0300

    1

---
+++ Sasha/19-21/20_2528.py
@@ -0,0 +1,15 @@
+def f(first,second,turn):
+    if 70<=first*second:#признак конца игры
+        if turn == 3 and first*second<=110:
+            return True
+        else:
+            return False
+    if turn > 3:
+        return False
+    if turn%2 == 0:#Снегурочка
+        return f(first+1,second,turn+1) or f(first*2,second,turn+1)  or f(first,second*2,turn+1) or f(first,second+1,turn+1)
+    else:#Дед мороз
+        return f(first+1,second,turn+1) and f(first*2,second,turn+1)  and f(first,second*2,turn+1) and f(first,second+1,turn+1)
+for  S in range(1,66):
+    if f(5,S,0):
+        print(S)
